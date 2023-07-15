7e33cd2cdad65d1aaab5a0fd4ed1f4301fc90c5b
kharl112 <kharl.dms112@gmail.com>
2023-07-15 12:15:55 +0800

    fixed project dir and added exec arg

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # update and install lxc (if doesn't exists)
+
 apt update
 apt install -y lxc
 
@@ -14,7 +15,7 @@ CONTAINER_NAME="${1:-$ADJECTIVE-$ANIMAL}"
 echo CONTAINER-NAME: $CONTAINER_NAME
 
 #set -ex
-PROJECT_DIR=`pwd | grep -o "\w*$"`
+PROJECT_DIR=`pwd | grep -o "\/[A-Z|a-z|0-9|\-]*" | tail -n 1`
 CONTAINER_HOME=/home/apps
 CONTAINER_DIR=/var/lib/lxc/$CONTAINER_NAME/rootfs$CONTAINER_HOME
 
@@ -27,5 +28,17 @@ lxc-start $CONTAINER_NAME
 mkdir -p $CONTAINER_DIR 
 cp $PWD $CONTAINER_DIR -R  
 
+#execute setup
 lxc-attach -n $CONTAINER_NAME -- $CONTAINER_HOME/$PROJECT_DIR/setup.sh
+
+if [ -z "$2" ]
+  then
+    echo "No directory supplied for project or executable"
+  else
+    echo "executing setup.sh from your supplied directory"
+    PR=`echo $2 | grep -o "\/[A-Z|a-z|0-9|\-]*" | tail -n 1`
+    cp $2 $CONTAINER_DIR -R
+    lxc-attach -n $CONTAINER_NAME -- $CONTAINER_HOME/$PR/setup.sh
+fi
+
 echo end time: `date`
