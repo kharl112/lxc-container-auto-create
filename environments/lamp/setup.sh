980548ced8f0297fbbb40440511a0c31a47d5200
kharl112 <kharl.dms112@gmail.com>
2023-07-22 09:05:03 +0800

    added environment lamp

---
+++ environments/lamp/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt update
+apt upgrade
+
+#NOTE: make sure dump all of your data before install mariadb again
+apt install apache2 mariadb-server mariadb-client
+
+#install deps
+apt install php libapache2-mod-php php-mysql php-fpm php-cli php-xml php-xmlrpc curl php-curl lynx mc
+
+# setup mysql 
+mysql_secure_installation
+
+#configure apache2
+a2enmod php || true
+
+#create a info page for php 
+echo "<?php phpinfo(); ?>" > /var/www/html/pinfo.php
+
+#fix permissions
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
