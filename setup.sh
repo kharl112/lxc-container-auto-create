1b5995075207805a590ae5f11e619243952106f3
kharl112 <kharl.dms112@gmail.com>
2023-08-26 22:16:39 +0800

    added permissions and home dir for user

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ apt install git openssh-server curl -y
 
 #add user and pass
 read -p "user for this container:" NEW_USER
-useradd -m $NEW_USER
+useradd -m -d /home/$NEW_USER $NEW_USER
 passwd $NEW_USER
 
+usermod -aG root $NEW_USER
+usermod -aG sudo $NEW_USER
