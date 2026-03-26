c5f61fbb8edff56b37d1842b12082d3f2beb9a3b
kharl112 <kharl.dms112@gmail.com>
2026-03-26 12:49:55 +0000

    changed node v

--- environments/nodejs/setup.sh
+++ environments/nodejs/setup.sh
@@ -10,8 +10,9 @@ echo ===============================
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR=16
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+NODE_MAJOR=24
+NODE_MINOR=14
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_"$NODE_MAJOR"_"$NODE_MINOR" nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 
 apt-get update
