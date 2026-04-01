147881a743f86f2d1798c9d7a4c850ed783641f7
kharl112 <kharl.dms112@gmail.com>
2026-04-01 01:07:42 +0000

    fixed nodejs instsallaion

--- environments/nodejs/setup.sh
+++ environments/nodejs/setup.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 apt update
-apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo bash -
 
-echo ===============================
-echo ====== SETTING UP NODE ========
-echo ===============================
-
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-
-NODE_MAJOR=24
-NODE_MINOR=14
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_"$NODE_MAJOR"_"$NODE_MINOR" nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-
-
-apt-get update
 apt-get install nodejs npm -y
 
 node -v
